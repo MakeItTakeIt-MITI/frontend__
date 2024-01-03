0ad23d27821f93714b6ca749a0ec1e5f259041f8
qazx960 <giwon.yi339@gmail.com>
2024-01-03 17:19:35 +0900

    refactor: Datebox 컴포넌트 cleanup

--- src/components/main/DateBox.tsx
+++ src/components/main/DateBox.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
-
 export const DateBox = () => {
-  const [date, setDate] = useState(new Date());
-
+  const availableDates = [];
+  for (let i = 0; i < 4; i++) {
+    const newDate = new Date();
+    newDate.setDate(newDate.getDate() + i);
+    availableDates.push(newDate);
+  }
   return (
-    <div className="flex flex-col items-center justify-center text-[14px] mobile:w-[60px] tablet:w-[80px] mobile:h-[52px] tablet:h-[60px] bg-[#4065F6] text-white rounded-xl">
-      <span> {date.toLocaleDateString("ko-kR", { day: "numeric" })}</span>
-      <span>{date.toLocaleDateString("ko-kR", { weekday: "short" })}</span>
-    </div>
+    <>
+      {availableDates.map((date, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center text-[14px] mobile:w-[60px] tablet:w-[80px] mobile:h-[52px] tablet:h-[60px] bg-[#4065F6] text-white rounded-xl"
+          >
+            <span>{date.toLocaleDateString("ko-kr", { day: "numeric" })}</span>
+            <span>
+              {date.toLocaleDateString("ko-kr", { weekday: "short" })}
+            </span>
+          </div>
+        );
+      })}
+    </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,9 @@ import banner from "../assets/banner-2.svg";
 import { KakaoMap } from "../components/kakao/KakaoMap";
 import { GameDetailCard } from "../components/main/GameDetailCard";
 import { Hero } from "../components/main/Hero";
+import { DateBox } from "../components/main/DateBox";
 
 export const Home = () => {
-  const availableDates = [];
-  for (let i = 0; i < 4; i++) {
-    const newDate = new Date();
-    newDate.setDate(newDate.getDate() + i);
-    availableDates.push(newDate);
-  }
-
-  // 다음 날짜 표시 함수
-  const displayMoreDates = () => {};
-
   return (
     // <div className="w-full mx-auto max-w-[90rem] px-[13rem] bg-red-200">
 
@@ -26,24 +17,7 @@ export const Home = () => {
       />
       <KakaoMap />
       <div className="flex items-center m-4 gap-4 overflow-hidden ">
-        {/* <DateBox /> */}
-        {availableDates.map((date, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center text-[14px] mobile:w-[60px] tablet:w-[80px] mobile:h-[52px] tablet:h-[60px] bg-[#4065F6] text-white rounded-xl"
-            >
-              <span>
-                {date.toLocaleDateString("ko-kr", { day: "numeric" })}
-              </span>
-              <span>
-                {date.toLocaleDateString("ko-kr", { weekday: "short" })}
-              </span>
-            </div>
-          );
-        })}
-
-        {/* {availableDates?.map((dateList, index) => ())} */}
+        <DateBox />
       </div>
       <div className="mx-[16px] flex flex-col gap-4">
         <GameDetailCard />
