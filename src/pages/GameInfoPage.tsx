b4d784a5c2c3004a4be80eedd30720ed90c44134
qazx960 <giwon.yi339@gmail.com>
2024-01-05 20:34:26 +0900

    feat: 호스트 정보 UI

--- src/pages/GameInfoPage.tsx
+++ src/pages/GameInfoPage.tsx
@@ -2,6 +2,8 @@ import courtbg from "../assets/court.svg";
 import markerSvg from "../assets/Map_Pin.svg";
 import phoneSvg from "../assets/Phone.svg";
 import peopleSvg from "../assets/people.svg";
+import badge from "../assets/authentication-badge.svg";
+import reviewStar from "../assets/star-review.png";
 export const GameInfoPage = () => {
   return (
     <div className="mobile:pt-[2rem] ">
@@ -33,7 +35,55 @@ export const GameInfoPage = () => {
         </div>
       </div>
       <hr className="h-[8px] w-full bg-gray-200" />
-      <div className="mobile:px-[16px] mobile:py-[18px]"></div>
+      <div className="flex flex-col gap-2 mobile:px-[16px] mobile:py-[18px] ">
+        <p className="text-[#222] font-bold">호스트 소개</p>
+        <div className="flex gap-2">
+          <div className="rounded-[50%] bg-gray-300 w-[40px] h-[40px]"></div>
+          <div className="flex flex-col">
+            <div className="flex items-center gap-1.5">
+              <p className="text-[#444] font-bold">어니언수제어묵 님</p>
+              <img src={badge} alt="user verified badge" />
+            </div>
+            <div className="flex gap-0.5 w-[250px] text-[14px] text-[#222]">
+              <img
+                src={reviewStar}
+                alt="review star"
+                className="w-[16px] h-[16px]"
+              />
+              <img
+                src={reviewStar}
+                alt="review star"
+                className="w-[16px] h-[16px]"
+              />
+              <img
+                src={reviewStar}
+                alt="review star"
+                className="w-[16px] h-[16px]"
+              />
+              <img
+                src={reviewStar}
+                alt="review star"
+                className="w-[16px] h-[16px]"
+              />
+              <img
+                src={reviewStar}
+                alt="review star"
+                className="w-[16px] h-[16px]"
+              />
+
+              <p className="ml-[4px]">5.0</p>
+              <p className="ml-[8px] underline">후기 7</p>
+            </div>
+          </div>
+        </div>
+        <p className="text-[#666]">
+          옥정 호수 공원 농구 코트에서 운동하다가 날씨가 추워져서 체육관을 잡고
+          운영하고있습니다. 나이, 키, 성별 상관 없습니다. 5대 5 잘 모르시는
+          분들도 환영합니다. 즐겁게 농구하는 즐농팀입니다. 과격하고 승리에
+          집착하시는 분들은 사양합니다.
+        </p>
+      </div>
+      <hr className="h-[8px] w-full bg-gray-200" />
     </div>
   );
 };
