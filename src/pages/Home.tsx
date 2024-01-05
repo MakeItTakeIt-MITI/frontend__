e4aa5e2b9d26aa30fc0a79c1da86db14b8c2e020
qazx960 <giwon.yi339@gmail.com>
2024-01-05 21:34:08 +0900

    feat: 모바일 및 웹 반응형 디자인

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { KakaoMap } from "../components/kakao/KakaoMap";
 import { GameDetailCard } from "../components/main/GameDetailCard";
 import { Hero } from "../components/main/Hero";
 import { DateBox } from "../components/main/DateBox";
+import { AdvertisementBanner } from "../components/AdvertisementBanner";
 
 export const Home = () => {
   return (
@@ -29,13 +30,7 @@ export const Home = () => {
         <GameDetailCard />
       </div>
       {/* <SectionTitle title="⚡ 마감 ️12시간 전 매칭" /> */}
-      <div className="bg-gray-200 relative mobile:w-screen mobile:px-[4rem] mobile:h-[40px] tablet:w-[1024px] tablet:h-[180px] border border-2-[#E8E8E8] rounded-xl my-[40px]">
-        <div className="absolute top-[16px] right-[16px] w-[60px] h-[32px] rounded-[4px] bg-[#FED500]">
-          <span className="text-center text-white font-bold flex items-center justify-center h-full">
-            Ad
-          </span>
-        </div>
-      </div>
+      <AdvertisementBanner />
     </div>
   );
 };
