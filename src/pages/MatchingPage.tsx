03038d6cc1fde4cc654faa8ecf997a748a099615
qazx960 <giwon.yi339@gmail.com>
2024-01-05 22:22:49 +0900

    feat: 매칭 페이지 생성

--- src/pages/MatchingPage.tsx
+++ src/pages/MatchingPage.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import left_arrow from "../assets/Chevron_Left.png";
 
 export const MatchingPage = () => {
-  return <div>MatchingPage</div>;
+  return (
+    <div>
+      <div className="py-[10px] relative">
+        <img src={left_arrow} alt="left arrow" className="absolute px-[13px]" />
+        <h4 className="text-center font-bold">매치 참가</h4>
+      </div>
+      <hr />
+    </div>
+  );
 };
