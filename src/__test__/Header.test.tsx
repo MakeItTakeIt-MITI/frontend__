79d19a49843ff40e417a4b29a98340ce15c912b5
qazx960 <giwon.yi339@gmail.com>
2024-01-05 11:54:26 +0900

    test: testing

---
+++ src/__test__/Header.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/extend-expect";
+
+// test("username input should be rendered", () => {
+//   render(<Header />);
+//   const userInput = screen.getByPlaceholderText(/username/i);
+//   expect(userInput).toBeInTheDocument();
+// });
+test("demo", () => {
+  expect(true).toBe(true);
+});
