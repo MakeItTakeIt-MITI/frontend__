edebf36a70e3aef587e33c2863e5d86b141e72d3
qazx960 <giwon.yi339@gmail.com>
2024-01-10 12:18:05 +0900

    feat: 경기 운영 페이지 생성

--- src/pages/GameOperate.tsx
+++ src/pages/GameOperate.tsx
@@ -2,9 +2,14 @@ import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/useAuthStore";
 import { useEffect } from "react";
 
+import mitiLogo from "../assets/MITI_logo.svg";
+import backArrow from "../assets/Chevron_Left.png";
+
 export const GameOperate = () => {
   const { isLoggedIn } = useAuthStore();
   const navigate = useNavigate();
+
+  const navigatePrev = () => navigate(-1);
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -12,5 +17,18 @@ export const GameOperate = () => {
     }
   }, []);
 
-  return <div>game</div>;
+  return (
+    <div className="mb-[4rem] ">
+      <button className="p-4" onClick={navigatePrev}>
+        <img src={backArrow} alt="back arrow" />
+      </button>
+      <hr className="w-full" />
+      <div className="flex items-center flex-col px-[16px]  py-[24px]">
+        <div className="mb-[36px] flex flex-col gap-2">
+          <img src={mitiLogo} alt="miti logo" />
+          <h5 className="text-[14px] text-[#1c1c1c]">Make it, Take it!</h5>
+        </div>
+      </div>
+    </div>
+  );
 };
