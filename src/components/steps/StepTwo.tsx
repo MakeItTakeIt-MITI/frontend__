df0bd0ae115c97908ffd874d037e56996e90f646
qazx960 <giwon.yi339@gmail.com>
2024-01-10 17:23:24 +0900

    feat: step 2 수정, 경기 일정

--- src/components/steps/StepTwo.tsx
+++ src/components/steps/StepTwo.tsx
@@ -10,33 +10,59 @@ export const StepTwo = () => {
         <h4 className="text-[24px] font-bold">경기 일정을 입력해주세요.</h4>
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="address" className="text-[12px] text-[#1c1c1c]">
-          경기 시작
+        <label htmlFor="start_date" className="text-[12px] text-[#1c1c1c]">
+          경기 시작 날짜
         </label>
-        <div className="flex">
-          <input
-            type="text"
-            id="address"
-            readOnly
-            className=" h-[58px] p-4 bg-[#F7F7F7] w-full"
-            placeholder="경기장 주소를 입력해주세요."
-            {...register("address", {
-              required: true,
-            })}
-          />
-          <button className="w-[70px] bg-[#4065F6] text-white">검색</button>
-        </div>
+
+        <input
+          type="date"
+          id="start_date"
+          readOnly
+          className=" h-[58px] p-4 bg-[#F7F7F7] w-full"
+          {...register("start_date", {
+            required: true,
+          })}
+        />
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="address_detail" className="text-[12px] text-[#1c1c1c]">
-          상세 주소
+        <label htmlFor="start_time" className="text-[12px] text-[#1c1c1c]">
+          경기 시작 시간
         </label>
         <input
-          type="text"
-          id="address_detail"
+          type="time"
+          id="start_time"
           className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
-          placeholder="상세 주소를 입력해주세요."
-          {...register("address_detail", {
+          step={900}
+          {...register("start_time", {
+            required: true,
+          })}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="end_date" className="text-[12px] text-[#1c1c1c]">
+          경기 종료 날짜
+        </label>
+
+        <input
+          type="date"
+          id="end_date"
+          readOnly
+          className=" h-[58px] p-4 bg-[#F7F7F7] w-full"
+          {...register("end_date", {
+            required: true,
+          })}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="end_time" className="text-[12px] text-[#1c1c1c]">
+          경기 종료 시간
+        </label>
+        <input
+          type="time"
+          id="end_time"
+          className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
+          step={900}
+          {...register("end_time", {
             required: true,
           })}
         />
