8ce7e6b3f3c74904ccba673855af43db80d64d86
qazx960 <giwon.yi339@gmail.com>
2024-01-10 11:26:10 +0900

    feat: 인증번호 input 추가

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -29,21 +29,6 @@ export const SignupForm = () => {
       className=" flex flex-col gap-6  w-full"
       onSubmit={handleSubmit(onSubmit)}
     >
-      {errors.email?.message && (
-        <p className="text-center text-red-500">{errors.email?.message}</p>
-      )}
-      {errors.password?.message && (
-        <p className="text-center text-red-500">{errors.password?.message}</p>
-      )}
-      {errors.password_check?.message && (
-        <p className="text-center text-red-500">
-          {errors.password_check?.message}
-        </p>
-      )}
-      {errors.nickname?.message && (
-        <p className="text-center text-red-500">{errors.nickname?.message}</p>
-      )}
-
       <div className="flex flex-col gap-2 relative">
         <label htmlFor="email" className="text-[12px] text-[#1c1c1c]">
           이메일
@@ -61,6 +46,9 @@ export const SignupForm = () => {
           중복확인
         </button>
       </div>
+      {errors.email?.message && (
+        <p className=" text-red-500">{errors.email?.message}</p>
+      )}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="password" className="text-[12px] text-[#1c1c1c]">
@@ -76,6 +64,9 @@ export const SignupForm = () => {
           })}
         />
       </div>
+      {errors.password?.message && (
+        <p className=" text-red-500">{errors.password?.message}</p>
+      )}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="password_check" className="text-[12px] text-[#1c1c1c]">
@@ -91,7 +82,9 @@ export const SignupForm = () => {
           })}
         />
       </div>
-
+      {errors.password_check?.message && (
+        <p className=" text-red-500">{errors.password_check?.message}</p>
+      )}
       <div className="flex flex-col gap-2 relative">
         <label htmlFor="nickname" className="text-[12px] text-[#1c1c1c]">
           닉네임
@@ -109,6 +102,10 @@ export const SignupForm = () => {
           중복확인
         </button>
       </div>
+      {errors.nickname?.message && (
+        <p className=" text-red-500">{errors.nickname?.message}</p>
+      )}
+
       <div className="flex flex-col gap-2">
         <label htmlFor="birthday" className="text-[12px] text-[#1c1c1c]">
           생년월일
@@ -128,9 +125,10 @@ export const SignupForm = () => {
         </label>
         <input
           className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
-          type="text"
+          type="tel"
           id="phone_number"
-          placeholder="휴대폰 번호를 입력해주세요."
+          pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+          placeholder="000-0000-0000"
           {...register("phone_number", {
             required: true,
           })}
@@ -139,7 +137,14 @@ export const SignupForm = () => {
           인증하기
         </button>
       </div>
-
+      <input
+        className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
+        type="number"
+        placeholder="인증번호"
+        {...register("confirmation_code", {
+          required: true,
+        })}
+      />
       <button
         type="submit"
         className=" h-[58px] p-4 bg-[#4065F6] rounded-lg text-white"
