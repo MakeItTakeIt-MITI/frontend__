411181f506760ffa40f4829431bc90fe44620417
qazx960 <giwon.yi339@gmail.com>
2024-01-15 13:30:18 +0900

    feat: 모바일 뷰 헤더 UI 추가

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -1,3 +1,30 @@
+import homeIcon from "../../assets/header_home_icon.svg";
+import gamesIcon from "../../assets/header_games_icon.svg";
+import profileIcon from "../../assets/header_profile_icon.svg";
+import viewAllIcon from "../../assets/header_all_icon.svg";
+import { Link } from "react-router-dom";
+
 export const MobileHeader = () => {
-  return <div>MobileHeader</div>;
+  return (
+    <nav className="tablet:hidden mobile:flex fixed bottom-0 w-full bg-white h-[4rem] border border-t-gray-200 z-10">
+      <div className="flex items-center w-full justify-center gap-16  p-4">
+        <Link to="/" className="flex flex-col gap-1 items-center ">
+          <img src={homeIcon} alt="home icon" />
+          <span className="text-[13px] text-[#969696]">홈</span>
+        </Link>
+        <Link to="/" className="flex flex-col gap-1 items-center">
+          <img src={gamesIcon} alt="games icon" />
+          <span className="text-[13px] text-[#969696]">경기</span>
+        </Link>
+        <Link to="/" className="flex flex-col gap-1 items-center">
+          <img src={profileIcon} alt="profile icon" />
+          <span className="text-[13px] text-[#969696]">프로필</span>
+        </Link>
+        <Link to="/" className="flex flex-col gap-1 items-center">
+          <img src={viewAllIcon} alt="view all icon" />
+          <span className="text-[13px] text-[#969696]">전체</span>
+        </Link>
+      </div>
+    </nav>
+  );
 };
--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,7 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
-import mitiLogo from "../../assets/MITI_logo.svg";
-import useAuthStore from "../../store/useAuthStore";
 import { TabletDesktopHeader } from "../header/TabletDesktopHeader";
+import { MobileHeader } from "../header/MobileHeader";
 
 // interface HeaderProps {
 //   isLoggedIn?: boolean;
@@ -24,7 +22,7 @@ export const Header = () => {
   //   }
   // };
   return (
-    <header>
+    <>
       {/* <header
       className="z-[99] mobile:h-[4rem] mobile:fixed mobile:flex mobile:justify-between mobile:px-[18px]  mobile:bottom-0 mobile:left-0 mobile:right-0 mobile:border mobile:border-t-gray-200 mobile:bg-white tablet:flex items-center   mobile:w-screen mobile:gap-4 
 tablet:static tablet:border-none tablet:top-0 tablet:justify-between tablet:bg-white  tablet:max-w-[90rem tablet:w-full  tablet:px-[4rem] laptop:px-[13rem] tablet:mx-auto tablet:text-[18px] desktop:text-[20px] tablet:h-[3.75rem] 
@@ -32,9 +30,9 @@ tablet:static tablet:border-none tablet:top-0 tablet:justify-between tablet:bg-w
     > */}
       {/* <div className="flex mobile:gap-6 tablet:items-center tablet:gap-5 "> */}
       {/* mobile */}
-
+      <MobileHeader />
       {/* tablet laptop desktop */}
       <TabletDesktopHeader />
-    </header>
+    </>
   );
 };
