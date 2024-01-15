89bb66d2dbb59615542d8689057fff57dc7df0fe
qazx960 <giwon.yi339@gmail.com>
2024-01-15 14:00:08 +0900

    feat: 모바일 헤더conditional rendering 추가, path에 따라 렌더링 수정

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -2,28 +2,47 @@ import homeIcon from "../../assets/header_home_icon.svg";
 import gamesIcon from "../../assets/header_games_icon.svg";
 import profileIcon from "../../assets/header_profile_icon.svg";
 import viewAllIcon from "../../assets/header_all_icon.svg";
-import { Link } from "react-router-dom";
+
+import homeIconColor from "../../assets/header_home_color.svg";
+import gamesIconColor from "../../assets/header_game_color.svg";
+import profileIconColor from "../../assets/header_profile_color.svg";
+// import viewIconColor from "../../assets/header_view_color.svg";
+
+import { Link, useLocation } from "react-router-dom";
 
 export const MobileHeader = () => {
+  const { pathname } = useLocation();
   return (
     <nav className="tablet:hidden mobile:flex fixed bottom-0 w-full bg-white h-[4rem] border border-t-gray-200 z-10">
       <div className="flex items-center w-full justify-center gap-16  p-4">
         <Link to="/" className="flex flex-col gap-1 items-center ">
-          <img src={homeIcon} alt="home icon" />
+          {pathname === "/" ? (
+            <img src={homeIconColor} alt="home icon" />
+          ) : (
+            <img src={homeIcon} alt="home icon" />
+          )}
           <span className="text-[13px] text-[#969696]">홈</span>
         </Link>
-        <Link to="/" className="flex flex-col gap-1 items-center">
-          <img src={gamesIcon} alt="games icon" />
+        <Link to="/operate" className="flex flex-col gap-1 items-center">
+          {pathname === "/operate" ? (
+            <img src={gamesIconColor} alt="games icon" />
+          ) : (
+            <img src={gamesIcon} alt="games icon" />
+          )}
           <span className="text-[13px] text-[#969696]">경기</span>
         </Link>
-        <Link to="/" className="flex flex-col gap-1 items-center">
-          <img src={profileIcon} alt="profile icon" />
+        <Link to="/login" className="flex flex-col gap-1 items-center">
+          {pathname === "/login" || pathname === "/signup" ? (
+            <img src={profileIconColor} alt="profile icon" />
+          ) : (
+            <img src={profileIcon} alt="profile icon" />
+          )}
           <span className="text-[13px] text-[#969696]">프로필</span>
         </Link>
-        <Link to="/" className="flex flex-col gap-1 items-center">
+        <div className="flex flex-col gap-1 items-center">
           <img src={viewAllIcon} alt="view all icon" />
           <span className="text-[13px] text-[#969696]">전체</span>
-        </Link>
+        </div>
       </div>
     </nav>
   );
