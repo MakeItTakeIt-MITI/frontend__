d29c97492893f5f6b6e3de25168e34b11de219a1
qazx960 <giwon.yi339@gmail.com>
2024-01-17 13:05:52 +0900

    refactor: 이메일 및 닉네임 중복검사 API 수정

--- src/api/validation.ts
+++ src/api/validation.ts
@@ -1,25 +1,28 @@
 import { ValidationField } from "../interface/usersInterface";
 import axiosUrl from "../utils/axios";
 
-export const userValidation = async (data: ValidationField) => {
-
+export const userValidation = async (data: ValidationField, setValidEmail: React.Dispatch<React.SetStateAction<boolean>>, setValidNickname: React.Dispatch<React.SetStateAction<boolean>>) => {
     try {
-        const response = await axiosUrl.post('/users/signup-check/', data);
+        const response = await axiosUrl.post("/users/signup-check/", data);
 
         if (response.data.status_code === 200) {
             console.log(response.data);
 
-            const emailData = response.data.data.email
-            const nicknameData = response.data.data.nickname
+            const emailData = response.data.data.email;
+            const nicknameData = response.data.data.nickname;
 
             if (emailData && emailData.is_duplicated === true) {
-                alert('이미 존재하는 이메일입니다.');
+                setValidEmail(false);
+                alert("이미 존재하는 이메일입니다.");
             } else if (emailData && emailData.is_duplicated === false) {
-                alert('사용 가능한 이메일입니다.');
+                setValidEmail(true);
+                alert("사용 가능한 이메일입니다.");
             } else if (nicknameData && nicknameData.is_duplicated === true) {
-                alert('이미 존재하는 닉네임입니다.');
+                setValidNickname(false);
+                alert("이미 존재하는 닉네임입니다.");
             } else if (nicknameData && nicknameData.is_duplicated === false) {
-                alert('사용 가능한 닉네임입니다.');
+                setValidNickname(true);
+                alert("사용 가능한 닉네임입니다.");
             }
 
             return response.data;
@@ -27,5 +30,4 @@ export const userValidation = async (data: ValidationField) => {
     } catch (error) {
         console.log(error);
     }
-}
-
+};
