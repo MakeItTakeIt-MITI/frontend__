7e57c8b83cea5239ed51f7efe59c4f7dc63d1506
qazx960 <giwon.yi339@gmail.com>
2024-01-17 12:48:29 +0900

    feat: 이메일 및 닉넹미 중복검사 API 추가

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -1,42 +1,77 @@
 import { useForm } from "react-hook-form";
-import { userRegisterSchema } from "../../modals/userSignupSchema";
+import { emailSchema, userRegisterSchema } from "../../modals/userSignupSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { RegisterField } from "../../interface/usersInterface";
-import { userValidation } from "../../api/validation";
+import { RegisterField, ValidationField } from "../../interface/usersInterface";
+import axiosUrl from "../../utils/axios";
+
+import { useState } from "react";
 
 export const SignupForm = () => {
+  const [validEmail, setValidEmail] = useState(false);
+  const [validNickname, setValidNickname] = useState(false);
+
   const {
     register,
     handleSubmit,
     getValues,
+    control,
     formState: { errors },
   } = useForm<RegisterField>({ resolver: zodResolver(userRegisterSchema) });
 
-  const handleValidateEmail = () => {
-    userValidation({ email: getValues("email") });
+  const userValidation = async (data: ValidationField) => {
+    try {
+      const response = await axiosUrl.post("/users/signup-check/", data);
+
+      if (response.data.status_code === 200) {
+        console.log(response.data);
+
+        const emailData = response.data.data.email;
+        const nicknameData = response.data.data.nickname;
+
+        if (emailData && emailData.is_duplicated === true) {
+          setValidEmail(false);
+          alert("이미 존재하는 이메일입니다.");
+        } else if (emailData && emailData.is_duplicated === false) {
+          setValidEmail(true);
+          alert("사용 가능한 이메일입니다.");
+        } else if (nicknameData && nicknameData.is_duplicated === true) {
+          setValidNickname(false);
+          alert("이미 존재하는 닉네임입니다.");
+        } else if (nicknameData && nicknameData.is_duplicated === false) {
+          setValidNickname(true);
+          alert("사용 가능한 닉네임입니다.");
+        }
+
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
-  const handleValidateNick = () => {
+
+  const handleValidateEmail = () =>
+    userValidation({ email: getValues("email") });
+
+  const handleValidateNick = () =>
     userValidation({ nickname: getValues("nickname") });
 
-    // userValidation(nicknameValue);
-  };
-  const onSubmit = (data: RegisterField) => {
-    // console.log(JSON.stringify(data));
-    console.log(data);
-
-    // try {
-    //   userRegisterSchema.parse(data);
-    //   userSignup(data);
-    //   console.log(data);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+  const onSubmit = async (data: RegisterField) => {
+    try {
+      userRegisterSchema.parse(data);
+      // userSignup(data);
+      const response = await axiosUrl.post("/users/", data);
+      console.log(response.data);
+      return response.data;
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <form
-      className="flex flex-col gap-6  w-full "
       onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-6  mobile:w-full tablet:w-[600px]"
     >
       <div className="flex flex-col gap-2 relative">
         <label htmlFor="email" className="text-[12px] text-[#1c1c1c]">
@@ -46,21 +81,35 @@ export const SignupForm = () => {
           className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
           type="email"
           id="email"
+          disabled={validEmail ? true : false}
           placeholder="이메일을 입력해주세요."
           {...register("email", {
             required: true,
           })}
         />
+
         <button
-          onClick={handleValidateEmail}
+          onClick={
+            !validEmail ? handleValidateEmail : () => setValidEmail(false)
+          }
           type="button"
-          className="absolute right-2 bottom-2.5 text-[14px] text-white bg-[#4065F6] w-[81px] h-[36px] rounded-[8px]"
+          style={
+            !validEmail
+              ? { backgroundColor: "#4065f6" }
+              : { backgroundColor: "#E8E8E8" }
+          }
+          className="absolute right-2 bottom-2.5 text-[14px] text-white w-[81px] h-[36px] rounded-[8px]"
         >
-          중복확인
+          {!validEmail ? "중복확인" : "수정하기"}
         </button>
       </div>
       {errors.email?.message && (
         <p className=" text-red-500">{errors.email?.message}</p>
+      )}
+      {validEmail && (
+        <p className="text-green-400 text-[13px] font-[400]">
+          사용 가능한 이메일이에요!
+        </p>
       )}
 
       <div className="flex flex-col gap-2">
@@ -120,17 +169,26 @@ export const SignupForm = () => {
           className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
           type="text"
           id="nickname"
+          disabled={validNickname ? true : false}
           placeholder="닉네임을 입력해주세요."
           {...register("nickname", {
             required: true,
           })}
         />
+
         <button
-          onClick={handleValidateNick}
+          onClick={
+            !validNickname ? handleValidateNick : () => setValidNickname(false)
+          }
           type="button"
-          className="absolute right-2 bottom-2.5 text-[14px] text-white bg-[#4065F6] w-[81px] h-[36px] rounded-[8px]"
+          style={
+            !validNickname
+              ? { backgroundColor: "#4065f6" }
+              : { backgroundColor: "#E8E8E8" }
+          }
+          className="absolute right-2 bottom-2.5 text-[14px] text-white w-[81px] h-[36px] rounded-[8px]"
         >
-          중복확인
+          {!validNickname ? "중복확인" : "수정하기"}
         </button>
       </div>
       {errors.nickname?.message && (
@@ -156,20 +214,20 @@ export const SignupForm = () => {
         </label>
         <input
           className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
-          type="string"
+          type="number"
           id="phone_number"
           pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
-          placeholder="000-0000-0000"
-          {...register("phone_number", {
+          placeholder="번호를 입력해주세요."
+          {...register("phone", {
             required: true,
           })}
         />
-        <button
+        {/* <button
           type="button"
           className="absolute right-2 bottom-2.5 text-[14px] text-white bg-[#4065F6] w-[81px] h-[36px] rounded-[8px]"
         >
           인증하기
-        </button>
+        </button> */}
       </div>
       {/* <input
         className=" h-[58px] p-4 bg-[#F7F7F7] rounded-lg"
@@ -180,7 +238,13 @@ export const SignupForm = () => {
         })}
       /> */}
       <button
-        type="submit"
+        // type="submit"
+        // disabled={!emailValidation && !nickValidation}
+        // style={
+        //   !emailValidation
+        //     ? { backgroundColor: "#eee" }
+        //     : { backgroundColor: "#4065f6" }
+        // }
         className=" h-[58px] p-4 bg-[#4065F6] rounded-lg text-white"
       >
         가입하기
