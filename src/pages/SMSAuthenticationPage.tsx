ad8871de77a1d1cfda5cb7848c39b1f2b516fdbe
qazx960 <giwon.yi339@gmail.com>
2024-01-17 16:10:50 +0900

    refactor: UI수정

--- src/pages/SMSAuthenticationPage.tsx
+++ src/pages/SMSAuthenticationPage.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 export const SMSAuthenticationPage = () => {
-  const [requestSMS, setRequestSMS] = useState(false);
+  //   const [requestSMS, setRequestSMS] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleRequestAuthCode = () => {
-    setRequestSMS(true);
-  };
+  //   const handleRequestAuthCode = () => {
+  //     setRequestSMS(true);
+  //   };
 
-  const handleAuthenticateUser = () => {
-    setIsAuthenticated(true);
-  };
+  //   const handleAuthenticateUser = () => {
+  //     setIsAuthenticated(true);
+  //   };
   return (
     <div className="px-[13rem] h-screen flex items-center flex-col gap-4 justify-center">
       <h1 className="text-[24px] font-bold">SMS 인증번호를 확인해주세요.</h1>
@@ -20,25 +20,14 @@ export const SMSAuthenticationPage = () => {
           type="text"
           placeholder="SMS 인증번로 입력해주세요."
         />
-        {!requestSMS ? (
-          <div className="flex gap-2">
-            <button className="bg-[#E8E8E8]  rounded-xl w-[100px] text-sm ">
-              재전송 요청
-            </button>
-            <button className="bg-[#4065f6]  text-white rounded-xl w-[100px] text-sm ">
-              인증하기
-            </button>
-          </div>
-        ) : (
-          <div className="flex gap-2">
-            <button className="bg-[#E8E8E8]  rounded-xl w-[100px] text-sm ">
-              재전송 요청
-            </button>
-            <button className="bg-[#4065f6]  text-white rounded-xl w-[100px] text-sm ">
-              인증하기
-            </button>
-          </div>
-        )}
+        <div className="flex gap-2">
+          <button className="bg-[#E8E8E8]  rounded-xl w-[100px] text-sm ">
+            재전송 요청
+          </button>
+          <button className="bg-[#4065f6]  text-white rounded-xl w-[100px] text-sm ">
+            인증하기
+          </button>
+        </div>
       </div>
       {!isAuthenticated && (
         <p className="text-[#E92C2C] text-[13px] font-[400]">
