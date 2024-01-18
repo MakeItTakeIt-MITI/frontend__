ae5e7036d0eff94fa94b88a55eeeed8b068a86cb
qazx960 <giwon.yi339@gmail.com>
2024-01-18 17:09:41 +0900

    refactor: 로그아웃  -> local storage clear

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,23 +2,31 @@ import { LoginField, RegisterField } from "../interface/usersInterface";
 import axiosUrl from "../utils/axios"
 
 export const userLoginAuth = async (data: LoginField) => {
+    const response = await axiosUrl.post('/users/login/', data)
+    console.log(response.data);
     try {
-        const response = await axiosUrl.post('/users/login/', data)
         if (response.data.status_code === 200) {
-            console.log(response.data);
             const accessToken = response.data.data.token?.access
+            const refreshToken = response.data.data.token?.refresh
+            const userId = response.data.data.id
             localStorage.setItem("accessToken", accessToken)
+            localStorage.setItem("refreshToken", refreshToken)
+            localStorage.setItem('id', userId)
             return response.data
-        } else if (response.data.status_code === 400) {
-            console.log('올바르지 않은 요청');
-        } else if (response.data.status_code === 401) {
-            console.log('사용자 정보 불일치');
-        } else if (response.data.status_code === 403) {
-            console.log('미인증/탈퇴 사용자 로그인');
-
         }
     } catch (error) {
-        console.log(error);
+
+        if (response.data.status_code === 400) {
+            alert('올바르지 않은 요청');
+        } else if (response.data.status_code === 401) {
+            alert('사용자 정보 불일치');
+        } else if (response.data.status_code === 403) {
+            alert('미인증/탈퇴 사용자 로그인');
+
+        }
+        // console.log(error.message);
+
+        // throw error
 
     }
 }
