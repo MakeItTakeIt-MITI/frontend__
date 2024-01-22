98a9a7ece3ebbe50860ea6873665f3d8d03606b3
qazx960 <giwon.yi339@gmail.com>
2024-01-22 14:32:44 +0900

    feat: 모바일 회원가입 페이지 헤더 숨김

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -17,44 +17,46 @@ export const MobileHeader = () => {
 
   const { pathname } = useLocation();
   return (
-    <nav className="tablet:hidden mobile:flex fixed bottom-0 left-0 right-0 w-full bg-white h-[4rem] border border-t-gray-200 z-10 rounded-t-2xl">
-      <div className="flex items-center w-full justify-center gap-16  p-4">
-        <Link to="/" className="flex flex-col gap-1 items-center ">
-          {pathname === "/" ? (
-            <img src={homeIconColor} alt="home icon" />
-          ) : (
-            <img src={homeIcon} alt="home icon" />
-          )}
-          <span className="text-[13px] text-[#969696]">홈</span>
-        </Link>
-        <Link to="/operate" className="flex flex-col gap-1 items-center">
-          {pathname === "/operate" ? (
-            <img src={gamesIconColor} alt="games icon" />
-          ) : (
-            <img src={gamesIcon} alt="games icon" />
-          )}
-          <span className="text-[13px] text-[#969696]">경기</span>
-        </Link>
-        <Link to="/login" className="flex flex-col gap-1 items-center">
-          {pathname === "/login" ||
-          pathname === "/signup" ||
-          pathname === "/profile" ? (
-            <img src={profileIconColor} alt="profile icon" />
-          ) : (
-            <img src={profileIcon} alt="profile icon" />
-          )}
-          <span className="text-[13px] text-[#969696]">프로필</span>
-        </Link>
-        <button
-          type="button"
-          onClick={() => setDisplayTab(true)}
-          className="flex flex-col gap-1 items-center"
-        >
-          <img src={viewAllIcon} alt="view all icon" />
-          <span className="text-[13px] text-[#969696]">전체</span>
-        </button>
-      </div>
-      {displayTab && <Sidebar setDisplayTab={setDisplayTab} />}
-    </nav>
+    <>
+      {pathname === "/signup" ? null : (
+        <nav className="tablet:hidden mobile:flex fixed bottom-0 left-0 right-0 w-full bg-white h-[4rem] border border-t-gray-200 z-10 rounded-t-2xl">
+          <div className="flex items-center w-full justify-center gap-16  p-4">
+            <Link to="/" className="flex flex-col gap-1 items-center ">
+              {pathname === "/" ? (
+                <img src={homeIconColor} alt="home icon" />
+              ) : (
+                <img src={homeIcon} alt="home icon" />
+              )}
+              <span className="text-[13px] text-[#969696]">홈</span>
+            </Link>
+            <Link to="/operate" className="flex flex-col gap-1 items-center">
+              {pathname === "/operate" ? (
+                <img src={gamesIconColor} alt="games icon" />
+              ) : (
+                <img src={gamesIcon} alt="games icon" />
+              )}
+              <span className="text-[13px] text-[#969696]">경기</span>
+            </Link>
+            <Link to="/login" className="flex flex-col gap-1 items-center">
+              {pathname === "/login" || pathname === "/profile" ? (
+                <img src={profileIconColor} alt="profile icon" />
+              ) : (
+                <img src={profileIcon} alt="profile icon" />
+              )}
+              <span className="text-[13px] text-[#969696]">프로필</span>
+            </Link>
+            <button
+              type="button"
+              onClick={() => setDisplayTab(true)}
+              className="flex flex-col gap-1 items-center"
+            >
+              <img src={viewAllIcon} alt="view all icon" />
+              <span className="text-[13px] text-[#969696]">전체</span>
+            </button>
+          </div>
+          {displayTab && <Sidebar setDisplayTab={setDisplayTab} />}
+        </nav>
+      )}
+    </>
   );
 };
