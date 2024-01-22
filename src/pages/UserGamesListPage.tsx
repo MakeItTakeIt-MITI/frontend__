379a095099f08b86b62fd9a72767dbc78f81fdef
qazx960 <giwon.yi339@gmail.com>
2024-01-22 15:47:32 +0900

    feat: 필터탭 기능 추가, toggle 기능 추가

--- src/pages/UserGamesListPage.tsx
+++ src/pages/UserGamesListPage.tsx
@@ -1,27 +1,35 @@
 import { NavigateToPrevContainer } from "../components/NavigateToPrevContainer";
 import downarrow from "../assets/Chevron_Down_MD.svg";
+import { useState } from "react";
 export const UserGamesListPage = () => {
+  const [displayTab, setDisplayTab] = useState(false);
+
+  const handleToggleTab = () => {
+    setDisplayTab(!displayTab);
+  };
   return (
     <div>
       <NavigateToPrevContainer />
       <div className="flex justify-between p-4 ">
         <h4 className="text-[18px] font-bold">나의 매치 스케줄</h4>
         <div className="relative">
-          <button className="flex gap-4 ">
+          <button onClick={handleToggleTab} className="flex gap-4 ">
             <span>전체보기</span>
             <img src={downarrow} alt="down arrow" />
           </button>
-          <div
-            style={{
-              boxShadow:
-                "0px 11px 15px 0px rgba(0, 0, 0, 0.10), 0px 9px 46px 0px rgba(0, 0, 0, 0.06), 0px 24px 38px 0px rgba(0, 0, 0, 0.04)",
-            }}
-            className=" text-[14px] py-1 px-2 flex flex-col justify-around items-start bg-[#fff] absolute  right-1 w-[157px] h-[102px]"
-          >
-            <button>전체보기</button>
-            <button>게스트만 보기</button>
-            <button>호스트만 보기</button>
-          </div>
+          {displayTab && (
+            <div
+              style={{
+                boxShadow:
+                  "0px 11px 15px 0px rgba(0, 0, 0, 0.10), 0px 9px 46px 0px rgba(0, 0, 0, 0.06), 0px 24px 38px 0px rgba(0, 0, 0, 0.04)",
+              }}
+              className=" text-[14px] py-1 px-2 flex flex-col justify-around items-start bg-[#fff] absolute  right-1 w-[157px] h-[102px]"
+            >
+              <button>전체보기</button>
+              <button>게스트만 보기</button>
+              <button>호스트만 보기</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
