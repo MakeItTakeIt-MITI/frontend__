95f7b9a0675bdf288feee9e8f397ee4b81c52aff
qazx960 <giwon.yi339@gmail.com>
2024-01-22 15:45:06 +0900

    사용자 스캐쥴 페이지 필터 탭 UI

---
+++ src/components/GuestHostFilterBox.tsx
@@ -0,0 +1,9 @@
+import downArrow from "../assets/Chevron_Down_MD.svg"
+
+export const GuestHostFilterBox = () => {
+  return (
+        <div>       
+         <div>GuestHostFilterBox</div>
+    </div>
+  )
+}
--- src/pages/UserGamesListPage.tsx
+++ src/pages/UserGamesListPage.tsx
@@ -1,9 +1,29 @@
-import { NavigateToPrevContainer } from "../components/NavigateToPrevContainer"
-
+import { NavigateToPrevContainer } from "../components/NavigateToPrevContainer";
+import downarrow from "../assets/Chevron_Down_MD.svg";
 export const UserGamesListPage = () => {
   return (
     <div>
-        <NavigateToPrevContainer />
+      <NavigateToPrevContainer />
+      <div className="flex justify-between p-4 ">
+        <h4 className="text-[18px] font-bold">나의 매치 스케줄</h4>
+        <div className="relative">
+          <button className="flex gap-4 ">
+            <span>전체보기</span>
+            <img src={downarrow} alt="down arrow" />
+          </button>
+          <div
+            style={{
+              boxShadow:
+                "0px 11px 15px 0px rgba(0, 0, 0, 0.10), 0px 9px 46px 0px rgba(0, 0, 0, 0.06), 0px 24px 38px 0px rgba(0, 0, 0, 0.04)",
+            }}
+            className=" text-[14px] py-1 px-2 flex flex-col justify-around items-start bg-[#fff] absolute  right-1 w-[157px] h-[102px]"
+          >
+            <button>전체보기</button>
+            <button>게스트만 보기</button>
+            <button>호스트만 보기</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
