a95afff28ab696b3d137b39ef521bacf6edcdee1
qazx960 <giwon.yi339@gmail.com>
2024-01-23 23:46:55 +0900

    feat: 사용자 정보 zustand store 생성

---
+++ src/store/useUserDataStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface UserIdField {
+    userId: number | null;
+    setUserId: (userId: number) => void;
+}
+
+const useUserDataStore = create(
+    persist<UserIdField>(
+        (set) => ({
+            userId: null,
+            setUserId: (userId: number) => {
+                set({ userId: userId });
+            }
+        }),
+        {
+            name: 'userData',
+        }
+    )
+);
+
+export default useUserDataStore;
