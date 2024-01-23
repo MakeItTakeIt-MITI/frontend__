d6653b2feda6af1331160bbb05286e000284ec64
qazx960 <giwon.yi339@gmail.com>
2024-01-23 14:35:06 +0900

    refactor: 뒤로가기 이미지지 width 수정

--- src/components/NavigateToPrevContainer.tsx
+++ src/components/NavigateToPrevContainer.tsx
@@ -1,17 +1,17 @@
-import { useNavigate } from "react-router-dom"
-import backArrow from "../assets/Chevron_Left.png"
+import { useNavigate } from "react-router-dom";
+import backArrow from "../assets/Chevron_Left.png";
 
 export const NavigateToPrevContainer = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const navigatePrev = () => navigate(-1);
+  const navigatePrev = () => navigate(-1);
 
   return (
     <div className="tablet:p-10">
-    <button className="mobile:block tablet:hidden p-4" onClick={navigatePrev}>
-      <img src={backArrow} alt="back arrow" />
-    </button>
-    <hr className="mobile:block tablet:hidden w-full" />
+      <button className="mobile:block tablet:hidden p-4" onClick={navigatePrev}>
+        <img src={backArrow} alt="back arrow" className="w-6" />
+      </button>
+      <hr className="mobile:block tablet:hidden w-full" />
     </div>
-  )
-}
+  );
+};
