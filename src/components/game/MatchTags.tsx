5411a047f4c2a89debdac8ab48c3edea20500d2c
qazx960 <giwon.yi339@gmail.com>
2024-02-02 20:29:30 +0900

    style: 태그별 스타일링 추가

--- src/components/game/MatchTags.tsx
+++ src/components/game/MatchTags.tsx
@@ -1,6 +1,22 @@
 interface TagProp {
   children: string;
+  backgroundColor: string;
+  textColor: string;
 }
-export const MatchTags = ({ children }: TagProp) => {
-  return <span className="text-[11px] font-[600]">{children}</span>;
+export const MatchTags = ({
+  children,
+  backgroundColor,
+  textColor,
+}: TagProp) => {
+  return (
+    <span
+      style={{
+        backgroundColor,
+        color: textColor,
+      }}
+      className="text-[11px] font-[600] rounded-[2px] p-1 "
+    >
+      {children}
+    </span>
+  );
 };
