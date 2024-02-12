6f712cdfda99d9b3dbd03bff2254bb3832f8d586
qazx960 <giwon.yi339@gmail.com>
2024-02-12 12:01:35 +0900

    fix: 삭제된 handleCopyClipBoard 함수 다시 추가

--- src/pages/UserJoinMatchPage.tsx
+++ src/pages/UserJoinMatchPage.tsx
@@ -38,6 +38,15 @@ export const UserJoinMatchPage = () => {
     console.log(data);
   };
 
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("클립보드에 링크가 복사되었습니다.");
+    } catch (e) {
+      alert("복사에 실패하였습니다");
+    }
+  };
+
   const handleShowModal = () => {
     setModal(true);
   };
