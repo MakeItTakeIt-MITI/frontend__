fde70f62aa23163e8ac1147724076b058dac25a9
qazx960 <giwon.yi339@gmail.com>
2024-02-19 02:32:30 +0900

    refactor: 카카오 지도 마커 제거, 텍스트만 유지

--- src/components/kakao/KakaoMap.tsx
+++ src/components/kakao/KakaoMap.tsx
@@ -61,30 +61,34 @@ export const KakaoMap = ({ allGamesData }) => {
             );
 
             // 결과값으로 받은 위치를 마커로 표시합니다
-            const marker = new window.kakao.maps.Marker({
-              map: kakaoMap,
-              position: coords,
-            });
+            // const marker = new window.kakao.maps.Marker({
+            //   map: kakaoMap,
+            //   position: coords,
+            //   clickable: true,
+            // });
             const iwRemoveable = true;
 
             // 인포윈도우로 장소에 대한 설명을 표시합니다
             const infowindow = new window.kakao.maps.InfoWindow({
               content: `
-              <div class="flex items-center justify-around w-[169px] h-[62px] p-[12px] rounded-xl shadow-lg  ">
-              <div class="">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-              <circle opacity="0.6" cx="16" cy="16" r="16" fill="#C1E1FF"/>
-              <path d="M17.945 24.93C20.1238 23.0125 24 19.095 24 15.5C24 13.5109 23.2098 11.6032 21.8033 10.1967C20.3968 8.79018 18.4891 8 16.5 8C14.5109 8 12.6032 8.79018 11.1967 10.1967C9.79018 11.6032 9 13.5109 9 15.5C9 19.095 12.875 23.0125 15.055 24.93C15.4526 25.2849 15.967 25.4811 16.5 25.4811C17.033 25.4811 17.5474 25.2849 17.945 24.93ZM14 15.5C14 14.837 14.2634 14.2011 14.7322 13.7322C15.2011 13.2634 15.837 13 16.5 13C17.163 13 17.7989 13.2634 18.2678 13.7322C18.7366 14.2011 19 14.837 19 15.5C19 16.163 18.7366 16.7989 18.2678 17.2678C17.7989 17.7366 17.163 18 16.5 18C15.837 18 15.2011 17.7366 14.7322 17.2678C14.2634 16.7989 14 16.163 14 15.5Z" fill="#4065F6"/>
-             </svg>
-          </div>
-          <h2 class="text-[14px]">
-          ${match.title}
-          </h2>
-          </div>
-            </div>`,
-              removable: iwRemoveable,
+              <div class="max-w-[75px]  h-[56px] p-2  flex flex-col items-center justify-center   ">
+              <p class="text-[14px]  text-[#999]">${match.court.address_detail}</p>
+              <p class="font-bold">${match.fee}</p>
+            </div>
+              `,
+
+              // removable: iwRemoveable,
+              map: kakaoMap,
+              position: coords,
             });
-            infowindow.open(kakaoMap, marker);
+
+            infowindow.open(kakaoMap);
+
+            // 마커에 클릭이벤트를 등록합니다
+            // window.kakao.maps.event.addListener(marker, "click", function () {
+            //   // 마커 위에 인포윈도우를 표시합니다
+            //   infowindow.open(kakaoMap, marker);
+            // });
 
             // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
             kakaoMap.setCenter(coords);
@@ -100,6 +104,15 @@ export const KakaoMap = ({ allGamesData }) => {
       // className="block w-full  h-[450px]"
       className="tablet:w-full  tablet:h-[473px]  mobile:h-[300px]"
     ></div>
+  );
+};
+
+export const MapBox = ({ match }) => {
+  return (
+    <div className="max-w-[75px]  h-[56px] p-2  flex flex-col items-center justify-center   ">
+      <p className="text-[14px]  text-[#999]">${match.court.address_detail}</p>
+      <p className="font-bold">${match.fee}</p>
+    </div>
   );
 };
 
@@ -124,6 +137,10 @@ export const KakaoDetailContainer = ({ match }) => {
         </div>
         <h2 className="text-[14px]">{match.title}</h2>
       </div>
+      {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+              <circle opacity="0.6" cx="16" cy="16" r="16" fill="#C1E1FF"/>
+              <path d="M17.945 24.93C20.1238 23.0125 24 19.095 24 15.5C24 13.5109 23.2098 11.6032 21.8033 10.1967C20.3968 8.79018 18.4891 8 16.5 8C14.5109 8 12.6032 8.79018 11.1967 10.1967C9.79018 11.6032 9 13.5109 9 15.5C9 19.095 12.875 23.0125 15.055 24.93C15.4526 25.2849 15.967 25.4811 16.5 25.4811C17.033 25.4811 17.5474 25.2849 17.945 24.93ZM14 15.5C14 14.837 14.2634 14.2011 14.7322 13.7322C15.2011 13.2634 15.837 13 16.5 13C17.163 13 17.7989 13.2634 18.2678 13.7322C18.7366 14.2011 19 14.837 19 15.5C19 16.163 18.7366 16.7989 18.2678 17.2678C17.7989 17.7366 17.163 18 16.5 18C15.837 18 15.2011 17.7366 14.7322 17.2678C14.2634 16.7989 14 16.163 14 15.5Z" fill="#4065F6"/>
+             </svg> */}
     </>
   );
 };
