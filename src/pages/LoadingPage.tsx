e38798fee5c5ec506d18a031e05ef461eaec93e6
qazx960 <giwon.yi339@gmail.com>
2024-02-28 08:32:33 +0900

    style: top pixel 수정

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -2,7 +2,7 @@ import miti from "../assets/MITI_logo.svg";
 
 export const LoadingPage = () => {
   return (
-    <div className="fixed top-0 bottom-0 right-0 left-0 h-full">
+    <div className="fixed top-[60px] bottom-0 right-0 left-0 h-full">
       <div className="flex items-center justify-center h-full">
         <img src={miti} alt="MITO logo" />
       </div>
