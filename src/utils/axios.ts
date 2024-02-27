860e03cb568a7133de68012f456b02690ed6960d
qazx960 <giwon.yi339@gmail.com>
2024-02-28 01:30:43 +0900

    feat: react jwt 설치, axios interceptor 헤더에 refresh토큰 추가

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -12,13 +12,23 @@ const axiosUrl: AxiosInstance = axios.create({
 });
 
 
-axiosUrl.interceptors.request.use(function (config) {
+axiosUrl.interceptors.request.use(async function (config) {
     const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    // const accessTokenDuration = 4 * 60 * 60; // access token expiration time (4 hours)
+
     if (accessToken) {
         config.headers.Authorization = `Bearer ${accessToken}`;
     }
+
+    if (refreshToken) {
+        config.headers["Refresh"] = refreshToken;
+    }
+
+
     return config;
-});
+}
+);
 export default axiosUrl;
 
 
