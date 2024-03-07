5cd6edffede749aa8d35234b4761ff65766c3f37
qazx960 <giwon.yi339@gmail.com>
2024-03-07 18:04:06 +0900

    refactor: 회원가입 제출 버튼 disabled 기능 추가, 에러 메세지 출력

--- src/api/validation.ts
+++ src/api/validation.ts
@@ -3,10 +3,7 @@ import axiosUrl from "../utils/axios";
 export const userValidation = async (data: ValidationField) => {
     try {
         const response = await axiosUrl.post("/auth/signup-check", data);
-        if (response.data.status_code === 200) {
-            console.log(response.data);
-            return response.data;
-        }
+        return response.data
     } catch (error) {
         throw new Error
     }
