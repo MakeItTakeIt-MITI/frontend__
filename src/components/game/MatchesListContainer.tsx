97cd34dc6e68c515eadbca613a61ad818c2a15da
qazx960 <giwon.yi339@gmail.com>
2024-03-15 14:43:44 +0900

    feat: game status added

--- src/components/game/MatchesListContainer.tsx
+++ src/components/game/MatchesListContainer.tsx
@@ -1,5 +1,7 @@
 // import { Link } from "react-router-dom";
 import { GameDetailField } from "../../interface/gameInterface";
+import { CompletedTag, RecrutingTag } from "../../stories/Tags.stories";
+import { MatchTags } from "./MatchTags";
 
 interface GameDetailProp {
   game: GameDetailField;
@@ -17,6 +19,11 @@ export const MatchListDetail = ({
     >
       {/* <Link to={`/games/detail/${game.id}`} className=""> */}
       <div className="flex flex-col gap-1  ">
+        {game.game_status === "cancelled" ? (
+          <MatchTags {...CompletedTag.args} />
+        ) : (
+          <MatchTags {...RecrutingTag.args} />
+        )}
         <h2 className="font-bold text-[18px] truncate">{game.title} </h2>
         <p className="text-[14px] text-gray-500">
           {`${game.startdate} ${game.starttime.slice(
