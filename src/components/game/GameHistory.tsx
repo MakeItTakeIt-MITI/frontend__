f8d994d6e1cda725ff543c969466a6da26e1ff07
qazx960 <giwon.yi339@gmail.com>
2024-03-16 14:40:55 +0900

    fix: 나의 게스트 참여 리스트 API field 업데이트 반영

--- src/components/game/GameHistory.tsx
+++ src/components/game/GameHistory.tsx
@@ -4,16 +4,18 @@ import gameDetailBtn from "../../assets/More_Info_btn.svg";
 
 import {
   CompletedTag,
+  GuestTag,
   HostTag,
   RecrutingTag,
 } from "../../stories/Tags.stories";
-import { GameDetailField } from "../../interface/gameInterface";
+import { GameDetailField, GuestGameField } from "../../interface/gameInterface";
 
 interface HostGameProps {
   hostHistory: { data: [GameDetailField] };
 }
-interface GuestGameHistory {
-  guestHistory: { data: [GameDetailField] };
+
+interface GuestGameProps {
+  guestHistory: GuestGameField;
 }
 
 export const HostGameHistory = ({ hostHistory }: HostGameProps) => {
@@ -53,28 +55,29 @@ export const HostGameHistory = ({ hostHistory }: HostGameProps) => {
   );
 };
 
-export const GuestGameHistory = ({ guestHistory }: GuestGameHistory) => {
+export const GuestGameHistory = ({ guestHistory }: GuestGameProps) => {
+  console.log(guestHistory.data);
   return (
     <>
-      {guestHistory?.data.map((guest: GameDetailField) => {
+      {guestHistory?.data.map((guest) => {
         return (
           <Link
-            key={guest.id}
-            to={`/games/detail/${guest.id}`}
+            key={guest.game.id}
+            to={`/games/detail/${guest.game.id}`}
             className="mobile:w-full tablet:w-[480px]  flex   justify-between p-4 border border-[#E8E8E8] rounded-xl"
           >
             <div className=" flex flex-col gap-1">
               <div className="flex gap-2">
-                <MatchTags {...HostTag.args} />
-                {guest.game_status === "cancelled" ? (
+                <MatchTags {...GuestTag.args} />
+                {guest.game.game_status === "cancelled" ? (
                   <MatchTags {...CompletedTag.args} />
                 ) : (
                   <MatchTags {...RecrutingTag.args} />
                 )}
               </div>
-              <h4>{guest?.title}</h4>
+              <h4>{guest?.game.title}</h4>
               <p className="text-[14px] text-[#999]">
-                {`${guest.startdate} ${guest.starttime} ~ ${guest.endtime}`}
+                {`${guest.game.startdate} ${guest.game.starttime} ~ ${guest.game.endtime}`}
               </p>
             </div>
             <div>
