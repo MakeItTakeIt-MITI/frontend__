f987fcd350de67e43ac5931a7c2de63b7851050a
qazx960 <giwon.yi339@gmail.com>
2024-03-21 19:23:10 +0900

    hotfix: cluster level adjusted

--- src/components/kakao/kakao_map_controls.ts
+++ src/components/kakao/kakao_map_controls.ts
@@ -16,7 +16,7 @@ export const displayMap = () => {
     const options = {
 
         center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 4,
+        level: 5,
     };
 
     return new kakao.maps.Map(container, options);
@@ -281,7 +281,7 @@ export const clusterDisplayAllGames = (map: any, allGamesData: any, geocoder: an
     const clusterer = new kakao.maps.MarkerClusterer({
         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-        minLevel: 4 // 클러스터 할 최소 지도 레벨 
+        minLevel: 5 // 클러스터 할 최소 지도 레벨 
     });
 
 
