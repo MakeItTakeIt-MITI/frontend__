413ec5149c450593a7e8657ae26a76ad975c0768
qazx960 <giwon.yi339@gmail.com>
2024-03-25 20:10:39 +0900

    feat: 네이버 지도 라이브러리

--- src/components/naver/NaverMap.tsx
+++ src/components/naver/NaverMap.tsx
@@ -5,17 +5,37 @@ import {
   getGeolocation,
   renderMap,
 } from "./naver_map_controls";
+import { NavermapsProvider } from "react-naver-maps";
+import { Container as MapDiv, NaverMap, Marker } from "react-naver-maps";
 
 export const NaverMapEL = ({ allGamesData, searchAddress }) => {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [addressList, setAddressList] = useState([]);
-  useEffect(() => {
-    renderMap(latitude, longitude);
-    getGeolocation(setLatitude, setLongitude);
-    addressToLatLongCoord("서울 성동구 고산자로 253");
-    displayAllGames(setAddressList, allGamesData);
-    console.log(addressList);
-  }, [latitude, longitude, allGamesData]);
-  return <section id="map" className="w-full  h-[473px] relative" />;
+  // const [latitude, setLatitude] = useState(null);
+  // const [longitude, setLongitude] = useState(null);
+  // const [addressList, setAddressList] = useState([]);
+  // useEffect(() => {
+  //   renderMap(latitude, longitude);
+  //   getGeolocation(setLatitude, setLongitude);
+  //   addressToLatLongCoord("서울 성동구 고산자로 253");
+  //   displayAllGames(setAddressList, allGamesData);
+
+  //   console.log(addressList);
+  // }, [latitude, longitude, allGamesData]);
+  // const clientId = import.meta.env.VITE_NAVER_CLIENT_ID;
+
+  return (
+    <>
+      {/* <NavermapsProvider ncpClientId={clientId}> */}
+      <MapDiv
+        className="w-full h-[463px]"
+        // style={{
+        //   width: "100%",
+        //   height: "600px",
+        // }}
+      >
+        <NaverMap />
+      </MapDiv>
+      {/* </NavermapsProvider> */}
+    </>
+  );
+  // <section id="map" className="w-full  h-[473px] relative" />;
 };
