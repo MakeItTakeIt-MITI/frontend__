dad18f25c43d3d47e66dda31f72077de4dd84f73
qazx960 <giwon.yi339@gmail.com>
2024-03-28 17:56:02 +0900

    refactor: server URL updated

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 // import { useNavigate } from "react-router-dom";
 
-const baseUrl = 'https://api.makeittakeit.kr'
+const baseUrl = 'https://dev.makeittakeit.kr'
 
 
 
