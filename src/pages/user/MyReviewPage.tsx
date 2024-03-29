5ccb99883540bb108de2a491b9797b43858a13f4
qazx960 <giwon.yi339@gmail.com>
2024-03-29 14:08:40 +0900

    feat: my reviews page created

--- src/pages/user/MyReviewPage.tsx
+++ src/pages/user/MyReviewPage.tsx
@@ -1,9 +1,21 @@
 import { NavigateToPrevContainer } from "../../components/NavigateToPrevContainer";
+import { MyReviewContainer } from "../../components/game/MyReviewContainer";
 
 export const MyReviewPage = () => {
   return (
-    <section className="mt-4">
+    <section className="my-4">
       <NavigateToPrevContainer children="내 리뷰" />
+      <div className="flex flex-col gap-4 laptop:w-[500px] min-h-[735px]    mobile:w-full mx-auto laptop:border laptop:border-gray-300  laptop:py-10 laptop:px-12 mobile:p-4 rounded-lg">
+        <h1 className="mobile:hidden tablet:block text-center font-bold text-2xl">
+          내 리뷰
+        </h1>
+        <div className="flex flex-col gap-4 my-4 ">
+          <MyReviewContainer />
+          <MyReviewContainer />
+          <MyReviewContainer />
+          <MyReviewContainer />
+        </div>
+      </div>
     </section>
   );
 };
