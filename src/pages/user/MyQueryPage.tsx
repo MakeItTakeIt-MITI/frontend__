6fe34a511a4be149b3779f59c566bb51f4175ad4
qazx960 <giwon.yi339@gmail.com>
2024-03-29 12:46:05 +0900

    feat: 고객센터 문의 모달 추가

--- src/pages/user/MyQueryPage.tsx
+++ src/pages/user/MyQueryPage.tsx
@@ -1,21 +1,67 @@
 import { useForm } from "react-hook-form";
 import { NavigateToPrevContainer } from "../../components/NavigateToPrevContainer";
+import { useState } from "react";
+import Modal from "react-modal";
 
 export interface QueryField {
   title: string;
   content: string;
 }
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 export const MyQueryPage = () => {
+  const [modal, setModal] = useState(false);
+
   const { register, formState } = useForm<QueryField>();
 
   const isFormEmpty =
     !formState.dirtyFields.title || !formState.dirtyFields.content;
 
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
   return (
-    <section className="mt-4">
+    <section className="mt-4 ">
       <NavigateToPrevContainer children="문의하기" />
-      <form className="laptop:w-[500px] min-h-[735px]   mobile:w-full mx-auto  laptop:border border-gray-300  laptop:py-10 laptop:px-12 mobile:p-4 rounded-lg flex flex-col justify-between ">
+      {modal && (
+        <Modal
+          isOpen={openModal}
+          // onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div className="w-[361px] h-[185px] p-4 flex flex-col justify-between rounded-lg">
+            <h1 className="p-8 text-center font-bold">
+              문의 작성을 완료하였습니다.
+            </h1>
+            <div className="flex justify-center">
+              <button
+                onClick={closeModal}
+                className="bg-[#4065F6] text-white w-[100px] h-[36px] rounded-lg"
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
+      <form className="relative laptop:w-[500px] min-h-[735px]   mobile:w-full mx-auto  laptop:border border-gray-300  laptop:py-10 laptop:px-12 mobile:p-4 rounded-lg flex flex-col justify-between ">
         <h1 className="mobile:hidden tablet:block text-center font-bold text-2xl">
           문의하기
         </h1>
@@ -44,6 +90,8 @@ export const MyQueryPage = () => {
         </div>
 
         <button
+          onClick={openModal}
+          type="button"
           disabled={isFormEmpty}
           className={`w-full h-[55px] rounded-lg ${
             isFormEmpty
