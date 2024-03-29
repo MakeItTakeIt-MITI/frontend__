f587f9f048747c6e482bf198b07a24dd5539587f
qazx960 <giwon.yi339@gmail.com>
2024-03-29 13:30:17 +0900

    feat: FAQ box open 기능 구현

--- src/pages/user/MyQueryPage.tsx
+++ src/pages/user/MyQueryPage.tsx
@@ -1,24 +1,12 @@
 import { useForm } from "react-hook-form";
 import { NavigateToPrevContainer } from "../../components/NavigateToPrevContainer";
 import { useState } from "react";
-import Modal from "react-modal";
 import { DisplayModal } from "../../components/common/DisplayModal";
 
 export interface QueryField {
   title: string;
   content: string;
 }
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
 
 export const MyQueryPage = () => {
   const [modal, setModal] = useState(false);
