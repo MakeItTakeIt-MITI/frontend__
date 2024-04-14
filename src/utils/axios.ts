24a84cee3282a08fbf2b9b19e9534884dcfce37b
qazx960 <giwon.yi339@gmail.com>
2024-04-15 08:13:16 +0900

    feat: refresh token function added

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,6 @@
 import axios, { AxiosInstance } from "axios";
-// import { useNavigate } from "react-router-dom";
 
-// const baseUrl = 'https://api.makeittakeit.kr'
 const baseUrl = 'https://dev.makeittakeit.kr'
-
-
 
 const axiosUrl: AxiosInstance = axios.create({
     baseURL: baseUrl,
@@ -15,13 +11,17 @@ const axiosUrl: AxiosInstance = axios.create({
 
 });
 
-
 axiosUrl.interceptors.request.use((config) => {
+
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
         config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
+}, error => {
+    console.log(error.data.response, 'fail');
+
+    return Promise.reject(error);
 }
 
 );
@@ -29,19 +29,51 @@ axiosUrl.interceptors.request.use((config) => {
 axiosUrl.interceptors.response.use(
     (response) => response,
     async (error) => {
-        if (error.response && error.response.status) {
-            // localStorage.removeItem('accessToken');
-            // localStorage.removeItem('userLoginStatus')
-
-            // console.log(error.response);
-            return error.response
+        // const originalRequest = error.config;
+        console.log(error.data);
+        console.log(error.data.error_code === 501);
+        if (error.response.status === 401 && error.data.error_code === 501) {
+            console.log('401');
+            console.log('--------------');
+            try {
+                const refreshToken = localStorage.getItem('refreshToken'); // Retrieve the stored refresh token.
+                const response = await axios.post('https://dev.makeittakeit.kr/auth/refresh-token', {
+                    refresh: refreshToken,
+                });
+                const { access, refresh } = response.data.data
+                localStorage.setItem('accessToken', access)
+                localStorage.setItem('refreshToken', refresh)
+                console.log(response, 'succeed in refrshing token');
+                return response.data
+            } catch (newError) {
+                console.log('refresh token failed:', newError);
+                return Promise.reject(newError);
+            }
         }
     }
 )
 
 axios.interceptors.response.use()
 
-
 export default axiosUrl;
 
 
+// originalRequest._retry = true; // Mark the request as retried to avoid infinite loops.
+// try {
+//     const refresh = localStorage.getItem('refreshToken'); // Retrieve the stored refresh token.
+//     const refreshData = { 'refresh': refresh }
+//     // Make a request  your auth server to refresh the token.
+//     const response = await axios.post('https://dev.makeittakeit.kr/auth/refresh-token', {
+//         refreshData,
+//     });
+//     const { accessToken, refreshToken: newRefreshToken } = response.data;
+//     localStorage.setItem('accessToken', accessToken);
+//     localStorage.setItem('refreshToken', newRefreshToken);
+//     return response.headers.Authorization = `Bearer ${accessToken}`;
+// } catch (refreshError) {
+//     console.error('Token refresh failed:', refreshError);
+
+//     localStorage.removeItem('accessToken');
+
+//     return Promise.reject(refreshError);
+// }
