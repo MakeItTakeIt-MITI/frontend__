d9355e23395a66ad757fc8e113936278e8143234
qazx960 <giwon.yi339@gmail.com>
2024-04-18 16:50:50 +0900

    feat: 카카오 로그인 success/failure 응답처리, 로그인 유지 state  management

--- src/pages/auth/useKakaoLoginMutation.tsx
+++ src/pages/auth/useKakaoLoginMutation.tsx
@@ -1,7 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { oAuthKakaoLogin } from "../../api/auth";
+import useAuthStore from "../../store/useAuthStore";
+import useUserDataStore from "../../store/useUserDataStore";
+import { useNavigate } from "react-router-dom";
 
 export const useKakaoLoginMutation = (setModal: (arg: boolean) => void) => {
+  const { login } = useAuthStore();
+  const { setUserId } = useUserDataStore();
+  const navigate = useNavigate();
+
   return useMutation({
     mutationKey: ["kakao_login"],
     mutationFn: oAuthKakaoLogin,
@@ -12,6 +19,13 @@ export const useKakaoLoginMutation = (setModal: (arg: boolean) => void) => {
 
       if (response.status_code === 200) {
         console.log(response);
+
+        const { access, refresh } = response.data.token;
+        localStorage.setItem("accessToken", access);
+        localStorage.setItem("refreshToken", refresh);
+        login();
+        setUserId(response.data.id);
+        navigate("/");
       }
     },
   });
