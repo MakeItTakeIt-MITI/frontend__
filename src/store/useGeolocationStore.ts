dda6698c5fd9aa32bee4fb579af8c804f99e6de2
qazx960 <giwon.yi339@gmail.com>
2024-04-22 01:23:06 +0900

    feat: geolocation store created

---
+++ src/store/useGeolocationStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface GeolocationField {
+    location: { latitude: number; longitude: number };
+    setCurrentMyLocation: (arg: number, arg2: number) => void;
+}
+
+const useGeolocationStore = create(
+    persist<GeolocationField>(
+        (set) => ({
+            location: { latitude: 0, longitude: 0 },
+            setCurrentMyLocation: (latitude, longitude) => {
+                set({ location: { latitude, longitude } });
+            }
+        }),
+        {
+            name: 'geolocationData',
+        }
+    )
+);
+
+export default useGeolocationStore;
