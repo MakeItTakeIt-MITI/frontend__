522e0c429487363c9719702b5cb015e50fe39603
Jiwon Lee <giwon.yi339@gmail.com>
2024-05-16 02:01:34 +0900

    feat: footer created

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full bg-[#1A1E27]">
+      <div className="w-[1024px] mx-auto py-[15px] text-white">
+        @MakeItaTakeIt.kr
+      </div>
+    </footer>
+  );
+};
