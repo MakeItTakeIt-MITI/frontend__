271814ea17667c119ca57ab9bb28d3e127d7275f
Jiwon Lee <giwon.yi339@gmail.com>
2024-05-17 02:05:24 +0900

    feat: footer added

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./components/home/Header";
 import { AdvertisementBanner } from "./components/AdvertisementBanner";
 import { ScrollToTop } from "./components/common/ScrollToTop";
+import { Footer } from "./components/common/Footer";
 
 function App() {
   return (
-    <main className="mb-[19px]">
+    <>
       <ScrollToTop />
       <Header />
       <Outlet />
       <AdvertisementBanner />
-    </main>
+      <Footer />
+    </>
   );
 }
 
