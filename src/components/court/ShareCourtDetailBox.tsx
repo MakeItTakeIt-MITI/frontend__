89af958df85ba0ceb8b72d9dc64c0e5a14024b84
Jiwon Lee <giwon.yi339@gmail.com>
2024-05-17 12:27:24 +0900

    feat: share api added, component created

---
+++ src/components/court/ShareCourtDetailBox.tsx
@@ -0,0 +1,44 @@
+import shareIcon from "../../assets/court/share_icon.svg";
+
+export const ShareCourtDetailBox = ({ courtDetailData }) => {
+  const handleSharePage = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: `${courtDetailData?.data.name}`,
+          text: ` ${courtDetailData?.data.info}`,
+          url: window.location.href,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("사용중인 브라우저에서 공유하기가 지원되지 않습니다.");
+    }
+  };
+  return (
+    <div
+      style={{ scrollbarWidth: "thin" }}
+      className="w-full space-y-[16.5px] h-[177px] border border-[#E8E8E8] p-3 rounded-lg  overflow-y-auto"
+    >
+      <div className="flex justify-between items-center">
+        <div className="space-y-[12.5px]">
+          {" "}
+          <h2 className="text-lg font-bold leading-5 text-ellipsis text-zinc-800">
+            {courtDetailData?.data.name}
+          </h2>
+          <p className=" text-xs font-medium leading-4 text-ellipsis text-neutral-400">
+            {courtDetailData?.data.address}
+            {courtDetailData?.data.address_detail}
+          </p>
+        </div>
+        <button onClick={handleSharePage}>
+          <img src={shareIcon} alt="share icon" />
+        </button>
+      </div>
+      <p className=" text-xs font-medium leading-5 text-zinc-900">
+        {courtDetailData?.data.info}
+      </p>
+    </div>
+  );
+};
--- src/pages/court/CourtDetailPage.tsx
+++ src/pages/court/CourtDetailPage.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { useGetCourtListInfiniteQuery } from "../../hooks/courts/useGetCourtListInfiniteQuery";
 import { CourtHistoryListContainer } from "../../components/court/CourtHistoryListContainer";
 import shareIcon from "../../assets/court/share_icon.svg";
+import { ShareCourtDetailBox } from "../../components/court/ShareCourtDetailBox";
 
 export const CourtDetailPage = () => {
   const { id } = useParams();
@@ -24,27 +25,7 @@ export const CourtDetailPage = () => {
       <div className="flex laptop:flex-row mobile:flex-col gap-5 laptop:px-3 mobile:px-1 laptop:w-[981px] laptop:h-[745px]  mx-auto ">
         <div className="laptop:max-w-[431px]  mobile:w-full gap-[25px] w-full flex flex-col">
           <CourtDetailMap courtData={courtDetailData} />
-          <div
-            style={{ scrollbarWidth: "thin" }}
-            className="w-full space-y-[16.5px] h-[177px] border border-[#E8E8E8] p-3 rounded-lg  overflow-y-auto"
-          >
-            <div className="flex justify-between items-center">
-              <div className="space-y-[12.5px]">
-                {" "}
-                <h2 className="text-lg font-bold leading-5 text-ellipsis text-zinc-800">
-                  {courtDetailData?.data.name}
-                </h2>
-                <p className=" text-xs font-medium leading-4 text-ellipsis text-neutral-400">
-                  {courtDetailData?.data.address}
-                  {courtDetailData?.data.address_detail}
-                </p>
-              </div>
-              <img src={shareIcon} alt="share icon" />
-            </div>
-            <p className=" text-xs font-medium leading-5 text-zinc-900">
-              {courtDetailData?.data.info}
-            </p>
-          </div>
+          <ShareCourtDetailBox courtDetailData={courtDetailData} />
         </div>
         <div
           style={{ scrollbarWidth: "thin" }}
