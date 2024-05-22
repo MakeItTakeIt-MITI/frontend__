0eb3ccea4325c6122a5ca897a8f83b0f1fdeb6e9
Jiwon Lee <giwon.yi339@gmail.com>
2024-05-22 13:42:53 +0900

    refactor: halfstar review rating added

--- src/components/common/ReviewRating.tsx
+++ src/components/common/ReviewRating.tsx
@@ -1,5 +1,6 @@
 import star from "../../assets/star-review.png";
 import emptyStar from "../../assets/empty_star_icon.svg";
+import halfStar from "../../assets/halfStar.png";
 
 interface ReviewRatingProp {
   reviewRating?: number[];
@@ -11,6 +12,8 @@ export const ReviewRating: React.FC<ReviewRatingProp> = ({ reviewRating }) => {
       {reviewRating?.map((i, index) =>
         i === 1 ? (
           <img key={index} src={star} alt="star" className="size-4" />
+        ) : i === 0.5 ? (
+          <img key={index} src={halfStar} alt="half star" className="size-4" />
         ) : (
           <img
             key={index}
--- src/stories/Reviews.stories.tsx
+++ src/stories/Reviews.stories.tsx
@@ -43,3 +43,22 @@ export const NoReviews = Template.bind({});
 NoReviews.args = {
   reviewRating: [0, 0, 0, 0, 0],
 };
+export const FourAndHalfStars = Template.bind({});
+FourAndHalfStars.args = {
+  reviewRating: [1, 1, 1, 1, 0.5],
+};
+
+export const ThreeAndHalfStars = Template.bind({});
+ThreeAndHalfStars.args = {
+  reviewRating: [1, 1, 1, 0.5, 0],
+};
+
+export const TwoAndHalfStars = Template.bind({});
+TwoAndHalfStars.args = {
+  reviewRating: [1, 1, 0.5, 0, 0],
+};
+
+export const OneAndHalfStar = Template.bind({});
+OneAndHalfStar.args = {
+  reviewRating: [1, 0.5, 0, 0, 0],
+};
