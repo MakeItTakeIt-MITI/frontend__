c55ed76a97c26ac65f76f936115e26d5abfcfdbd
Jiwon Lee <giwon.yi339@gmail.com>
2024-06-11 12:43:20 +0900

    refactor: min height adjusted

--- src/app/routes/support/FAQPage.tsx
+++ src/app/routes/support/FAQPage.tsx
@@ -20,7 +20,7 @@ export const FAQPage = () => {
   return (
     <section className="laptop:my-[69px] mobile:my-0">
       <NavigateToPrevContainer children="FAQ" />
-      <div className="flex flex-col gap-4 laptop:w-[495px]     mobile:w-full mx-auto rounded-lg">
+      <div className="flex flex-col gap-4 laptop:w-[495px]  min-h-[720px]    mobile:w-full mx-auto rounded-lg">
         <h1 className="mobile:hidden tablet:block text-black text-[26px] font-bold">
           FAQ
         </h1>
