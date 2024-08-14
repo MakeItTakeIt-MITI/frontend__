1f1b84f3951174572912bf943a6daf7decea272d
Jiwon Lee <giwon.yi339@gmail.com>
2024-08-15 02:36:16 +0900

    feat: detail page marker created

--- src/components/game-details/GameDetailMap.tsx
+++ src/components/game-details/GameDetailMap.tsx
@@ -1,14 +1,23 @@
 import { useEffect } from "react";
+import marker from "../../assets/v11/detail-marker.svg";
 const { naver } = window;
 
 const GameDetailMap = () => {
   useEffect(() => {
-    new naver.maps.Map("map", {
+    const map = new naver.maps.Map("map", {
       // center: new naver.maps.LatLng(latitude, longitude),
       center: new naver.maps.LatLng(37.3595704, 127.105399),
       zoom: 13,
       pinchZoom: true,
       scrollWheel: true,
+    });
+
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(37.3595704, 127.105399),
+      map: map,
+      icon: {
+        content: `<img src=${marker} alt="marker" />`,
+      },
     });
   }, []);
   return (
---
+++ src/components/game-filter/TimeSlider.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TimeSlider = () => {
+  return (
+    <div className="flex items-center justify-center gap-[7px]">
+      {/* 오전 오후 */}
+      <div></div>
+      {/* 시 */}
+
+      {/* 분 */}
+    </div>
+  );
+};
+
+export default TimeSlider;
