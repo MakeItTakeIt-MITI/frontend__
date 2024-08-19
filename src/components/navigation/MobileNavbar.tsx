6468cf6e515601340c194bf8ae2f3b684e7e3792
Jiwon Lee <giwon.yi339@gmail.com>
2024-08-19 15:19:11 +0900

    feat: mobile navbar added

--- src/components/navigation/MobileNavbar.tsx
+++ src/components/navigation/MobileNavbar.tsx
@@ -1,16 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/v11/logo.svg";
 import hamburger from "../../assets/v11/hamburger.svg";
+import { useState } from "react";
+import { NAVITEMS } from "../../constants/navigation";
+import close from "../../assets/v11/mobile_close.svg";
 
 const MobileNavbar = () => {
+  const [displayTab, setDisplayTab] = useState(false);
+  const { pathname } = useLocation();
+
+  const handleOpenTab = () => {
+    setDisplayTab(!displayTab);
+  };
+
   return (
-    <nav className="sm:flex h-[3.75rem] w-full justify-between md:hidden px-[1.25rem] py-[1rem] bg-primary-black">
+    <nav className="relative sm:flex h-[3.75rem] w-full justify-between md:hidden px-[1.25rem] py-[1rem] bg-primary-black">
       <Link to="/">
         <img src={logo} alt="logo" />
       </Link>
-      <button type="button">
-        <img src={hamburger} alt="hamburger" />
+      <button type="button" onClick={handleOpenTab}>
+        {displayTab ? (
+          <img src={close} alt="close" />
+        ) : (
+          <img src={hamburger} alt="hamburger" />
+        )}
       </button>
+
+      {/* nav items */}
+      {displayTab && (
+        <div
+          onMouseLeave={() => setDisplayTab(false)}
+          className="z-[9999] absolute left-0 right-0 -bottom-[8.2rem]  bg-primary-black "
+        >
+          {NAVITEMS.map((nav) => (
+            <Link
+              to={nav.path}
+              style={{
+                color: pathname === `${nav.path}` ? "#7FEEF0" : "#fff",
+              }}
+              className="flex flex-col cursor-pointer font-[500] py-[.62rem] px-[1.31rem] text-primary-white w-full h-full "
+            >
+              {nav.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
