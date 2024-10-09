f7c694218841392291586f453d207913975c2ff9
Jiwon Lee <giwon.yi339@gmail.com>
2024-10-09 18:45:46 +0900

    refactor: courts landing page edited, animation added

--- src/components/landing/Courts.tsx
+++ src/components/landing/Courts.tsx
@@ -1,32 +1,57 @@
-import courts from "../../assets/v11/landing-court.png";
+import { Link } from "react-router-dom";
+import courts from "../../assets/v11/landing-courts.png";
+import { useInView } from "react-intersection-observer";
+import "./landing.css";
 
 const Courts = () => {
+  const { ref: imgRef, inView: imgInView } = useInView({
+    threshold: 0.7,
+    triggerOnce: true,
+  });
+  const { ref: contextRef, inView: contextInView } = useInView({
+    threshold: 0.7,
+    triggerOnce: true,
+  });
   return (
-    // <div className="w-full h-[800px] bg-secondary-black flex justify-center">
-    <div className="w-full h-[800px] bg-secondary-black flex justify-center  ">
+    <div className="sm:hidden md:flex justify-center h-[800px] bg-secondary-black   ">
       <div className="flex items-center ">
-        <div className="space-y-3">
-          <h1 className="text-primary-teal font-bold text-[18px]">
-            경기장 조회
-          </h1>
-          <h2 className="text-white text-[52px] font-bold">
-            우리 동네 농구 핫플레이스 <br />
-            지금 찾아보세요!
-          </h2>
-          <p className="text-[#E5E5E5] text-xl font-[400]">
-            우리 동네의 숨겨진 농구 경기장과 게스트를 모집 중인 경기를 <br />한
-            번에 조회하실 수 있습니다
-          </p>
-          <button
-            type="button"
-            className="w-[180px] h-[54px] bg-[#D4D4D4] text-[#262626] text-[18px] font-bold rounded-[14.286px]"
-          >
-            경기장 보러 가기
-          </button>
+        <div
+          ref={contextRef}
+          className={`${contextInView ? "toast" : ""} opacity-0 flex  items-center justify-start `}
+        >
+          <div className="space-y-9 text-left">
+            <div className="space-y-3">
+              <h1 className="text-primary-teal font-bold text-[18px]">
+                경기장 조회
+              </h1>
+              <h2 className="text-white  text-[52px] font-bold">
+                우리 동네 농구 핫플레이스 <br />
+                지금 찾아보세요!
+              </h2>
+            </div>
+            <p className="text-[#E5E5E5]  text-xl font-[400]">
+              우리 동네의 숨겨진 농구 경기장과 게스트를 모집 중인 경기를 <br />
+              한 번에 조회하실 수 있습니다
+            </p>
+
+            <button className="w-[180px] h-[54px] bg-[#D4D4D4] text-[#262626] font-bold rounded-[14.286px]">
+              <Link
+                to="/courts/list"
+                className=" w-full h-full text-center flex items-center justify-center"
+              >
+                경기장 보러 가기
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
-      <div>
-        <img src={courts} alt="courts landing" />
+      <div className={`flex items-end h-full  `}>
+        <img
+          ref={imgRef}
+          src={courts}
+          alt="games mobile page"
+          className={`${imgInView ? "slide-from-right" : ""}  opacity-0  h-[650px]`}
+        />
       </div>
     </div>
   );
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,6 +5,7 @@ import HeroMobile from "../components/landing/HeroMobile";
 
 import Games from "../components/landing/Games";
 import GamesMobile from "../components/landing/GamesMobile";
+import Courts from "../components/landing/Courts";
 
 const Landing = () => {
   // const userAgent = navigator.userAgent;
@@ -22,57 +23,8 @@ const Landing = () => {
       <HeroMobile />
       <Games />
       <GamesMobile />
-      {/* <Section
-        title="경기 조회"
-        subtitle="간편하게 경기를 조회하고 <br /> 바로 참여해보세요."
-        description="지도를 통해 내 주변의 경기를 빠르게 찾아보고 클릭 몇번으로 <br /> 간편하게 경기에 참여해보세요!"
-        buttonText="경기 보러 가기"
-        desktopImage={games}
-        mobileImage={mobile_games}
-        mobileGap="102px"
-        backgroundColor="dark-card"
-        reverse={true}
-        textPosition="right"
-        isButton={true}
-        path="/games/list"
-        sectionRef={gamesRef}
-        introView={gamesInView}
-      /> */}
-      {/* <Section
-        title="경기장 조회"
-        subtitle="우리 동네 농구 핫플레이스 <br /> 지금 찾아보세요!"
-        description=" 우리 동네의 숨겨진 농구 경기장과 게스트를 모집 중인 경기를 <br />한 번에 조회하실 수 있습니다"
-        buttonText="경기장 보러 가기"
-        desktopImage={courts}
-        mobileImage={mobile_courts}
-        mobileGap="62px"
-        backgroundColor="secondary-black"
-        reverse={false}
-        textPosition="left"
-        isButton={true}
-        path="/courts/list"
-        sectionRef={courtsRef}
-        introView={courtsInView}
-      /> */}
-      {/* <Section
-        title="경기 정산"
-        subtitle="번거로운 정산은 MITI가, <br /> 농구만 즐기세요!"
-        description="  수많은 게스트의 참가비 일일이 관리하기 힘드시죠? <br /> 이제 참가비는 MITI가 자동으로 정산해 드리니 농구만 즐기세요"
-        buttonText=""
-        desktopImage={payment}
-        mobileImage={mobile_payment}
-        mobileGap="113px"
-        backgroundColor="light-dark"
-        reverse={true}
-        textPosition="right"
-        isButton={false}
-        path=""
-        sectionRef={paymentsRef}
-        introView={introInView}
-      /> */}
-      {/* <Games /> */}
-      {/* <Courts /> */}
-      {/* <Payments /> */}
+      <Courts />
+
       <div className="h-[32.5rem] w-full pt-[6.12rem] pb-[8.88rem] bg-primary-black">
         <Footer />
       </div>
