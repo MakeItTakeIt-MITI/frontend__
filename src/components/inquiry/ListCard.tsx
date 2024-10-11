a14e67266074026a40b16aff352ebcd497e2c3d0
Jiwon Lee <giwon.yi339@gmail.com>
2024-10-11 15:38:42 +0900

    refactor: inquiry page UI edited

--- src/components/inquiry/ListCard.tsx
+++ src/components/inquiry/ListCard.tsx
@@ -16,9 +16,23 @@ const ListCard = ({ inquiry }: ListCardProps) => {
         onClick={handleDisplayModal}
         className="flex justify-between items-center text-white px-2.5 cursor-pointer"
       >
-        <h2 className="md:text-lg sm:text-sm font-normal">{inquiry.title}</h2>
-        <p className="md:text-sm sm:text-[10px] text-white font-[300] ">
-          {`${inquiry.created_at.slice(0, 4)}년 ${inquiry.created_at.slice(5, 7).padStart(2, "0")}월 ${inquiry.created_at.slice(8, 10).padStart(2, "0")}일`}
+        <div className="flex items-center gap-[3rem]">
+          <p className="text-sm font-[300 ]">
+            {`${inquiry.created_at.slice(0, 4)}년 ${inquiry.created_at.slice(5, 7).padStart(2, "0")}월 ${inquiry.created_at.slice(8, 10).padStart(2, "0")}일`}
+          </p>
+
+          <div className="flex items-center gap-[.3rem]">
+            {inquiry.num_of_answers > 0 && (
+              <p className="bg-[#7FEEF0] text-[#000] w-[50px] h-[21px] flex items-center justify-center text-[10px] font-[300] rounded-[100px]">
+                답변완료
+              </p>
+            )}
+            <p className="text-[18px] font-[400]">{inquiry.title}</p>
+          </div>
+        </div>
+
+        <p className="text-sm font-[300] truncate w-[109px] ">
+          {inquiry.nickname}
         </p>
       </div>
       <hr className="w-full h-[1px] text-[#525252]" />
