eea95ff11a8f2c85099e008e788f4690c595e809
Jiwon Lee <giwon.yi339@gmail.com>
2024-10-16 13:26:43 +0900

    refactor: text styling edited

--- src/components/landing/HeroContext.tsx
+++ src/components/landing/HeroContext.tsx
@@ -4,13 +4,13 @@ import appleStore from "../../assets/v11/apple-store-teal.svg";
 const HeroContext = () => {
   return (
     <div className="z-30  flex flex-col justify-end h-full  space-y-[2rem] sm:px-[1.5rem] md:pl-[22.5rem] md:pb-[8.44rem] sm:pb-[calc(5.87rem+0.75rem)] ">
-      <p className=" opacity-0 toast md:text-[#A3A3A3]   md:text-[40px] sm:text-[#EBFFFF] sm:text-[24px] font-bold  ">
+      <p className=" toast md:text-[#9EEFF0]   md:text-[32px] sm:text-[#9EEFF0] sm:text-[24px] font-bold  ">
         MITI 서비스 런칭
       </p>
-      <p className="opacity-0 toast text-[#fff]  md:text-[52px] sm:text-[24px]  font-bold  ">
+      <p className=" toast text-[#fff]  md:text-[52px] sm:text-[24px]  font-bold  ">
         농구 경기 더 쉽게, 더 즐겁게!
       </p>
-      <p className="opacity-0 toast text-[#fff]  sm:text-sm md:text-xl font-[400] ">
+      <p className=" toast text-[#fff]  sm:text-sm md:text-xl font-[400] ">
         번거로움은 빼고 농구에 재미를 더하세요! <br />
         지금 다운로드하고 오늘 퇴근 후에 농구 어떠세요?
       </p>
